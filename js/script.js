35037dad5bddc88462c30c6c9fdda55a63f7e7ec
Pablo Fernandez Grado <pablo93fernandez@gmail.com>
2025-09-30 09:58:14 +0200

    desarrollo de las pantallas de inicio, mas del museo, opciones y detalle de una obra, con header y footer en todas ellas.

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((data) => {
       document.getElementById("main-header").innerHTML = data;
       // Marcar el enlace activo en el menú de navegación después de cargar el header
-      const currentUrl = window.location.pathname
-        ? window.location.pathname
-        : "/index.html";
+      const currentUrl =
+        window.location.pathname !== "/"
+          ? window.location.pathname
+          : "/index.html";
       const navLinks = document.querySelectorAll("nav a");
 
       navLinks.forEach((link) => {
@@ -28,3 +29,54 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("main-footer").innerHTML = data;
     });
 });
+
+function activarCamara() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      const botonCamara = document.getElementById("boton-camara");
+      const video = document.getElementById("camara");
+      video.autoplay = true;
+      video.srcObject = stream;
+      botonCamara.style.display = "none";
+      video.style.display = "block";
+      setTimeout(() => {
+        window.location.href = "/pages/detalle-obra.html";
+      }, 5000);
+    })
+    .catch((err) => {
+      alert("No se pudo acceder a la cámara: " + err.message);
+    });
+}
+
+function playAudio(obra) {
+  const audioSrc = `../audios/descripcion_${obra}.wav`;
+  audioElement = document.createElement("audio");
+  audioElement.id = `audio-${obra}`;
+  audioElement.src = audioSrc;
+  audioElement.style.display = "none";
+  document.body.appendChild(audioElement);
+  audioElement.play();
+  audioElement.addEventListener("ended", function () {
+    pauseAudio(obra);
+  });
+  toggleIcon(obra, true);
+}
+function pauseAudio(obra) {
+  const audioElement = document.getElementById(`audio-${obra}`);
+  const isPlaying = !audioElement.paused;
+
+  if (isPlaying) {
+    audioElement.pause();
+  }
+  toggleIcon(obra, false);
+}
+
+function toggleIcon(obra, isPlaying) {
+  document.getElementById(`icon-play-${obra}`).style.display = isPlaying
+    ? "none"
+    : "inline";
+  document.getElementById(`icon-pause-${obra}`).style.display = isPlaying
+    ? "inline"
+    : "none";
+}
